b7da9923b8202b93d0c65dc22d11937336354748
Briana King <brianaking626@gmail.com>
2025-08-11 20:39:05 -0400

    genius test

--- functions/server.js
+++ functions/server.js
@@ -198,7 +198,8 @@ async function getLyrics(songTitle, songArtist) {
     // const encodedArtist = encodeURIComponent(songArtist);
     // const encodedTitle = encodeURIComponent(songTitle);
     // const url = `https://api.lyrics.ovh/v1/${encodedArtist}/${encodedTitle}`;
-    const Client = new Genius.Client();
+    console.log("GENIUS_KEY in Netlify:", process.env.GENIUS_API_KEY)
+    const Client = new Genius.Client(process.env.GENIUS_API_KEY);
 
     const query = `${songTitle} ${songArtist}`;
     const results = await Client.songs.search(query);
